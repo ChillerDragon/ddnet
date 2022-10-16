7f402304f58bb973b8f58f3377b93414cc61398f
ChillerDragon <ChillerDragon@gmail.com>
2022-10-16 10:14:05 +0200

    Add 0.7 client support
    While keeping 0.6 fully working and untouched
    This adds the option to connect via 0.7
    
    0.7 servers are now mixed into the server browser list.
    And users can also manually connect via 0.7
    using a connection url like so:
    
        ./DDNet "connect tw-0.7+udp://127.0.0.1:8303"
    
    This also adds a "Tee 0.7" section in the menu to pick a 0.7 skin.
    The config variables are prefixed with player7_ like for example
    player7_color_body

--- src/game/client/components/menus.h
+++ src/game/client/components/menus.h
@@ -24,6 +24,12 @@
 #include <game/client/ui.h>
 #include <game/voting.h>
 
+#include <game/client/render.h>
+
+#include <game/client/components/skins7.h>
+
+#include <game/client/lineinput.h>
+
 struct CServerProcess
 {
 	PROCESS m_Process;
@@ -227,6 +233,13 @@ protected:
 	bool m_NeedSendinfo;
 	bool m_NeedSendDummyinfo;
 	int m_SettingPlayerPage;
+
+	// 0.7 skins
+	int m_TeePartSelected;
+	bool m_RefreshSkinSelector;
+	const CSkins7::CSkin *m_pSelectedSkin;
+	bool m_SkinModified;
+	CLineInputBuffered<protocol7::MAX_SKIN_ARRAY_SIZE, protocol7::MAX_SKIN_LENGTH> m_SkinNameInput;
 
 	// for map download popup
 	int64_t m_DownloadLastCheckTime;
@@ -505,6 +518,11 @@ protected:
 	void RenderSettingsPlayer(CUIRect MainView);
 	void RenderSettingsDummyPlayer(CUIRect MainView);
 	void RenderSettingsTee(CUIRect MainView);
+	void RenderSettingsTee7(CUIRect MainView);
+	void RenderSettingsTeeCustom7(CUIRect MainView);
+	void RenderSettingsTeeBasic7(CUIRect MainView);
+	void RenderSkinSelection7(CUIRect MainView);
+	void RenderSkinPartSelection7(CUIRect MainView);
 	void RenderSettingsControls(CUIRect MainView);
 	void ResetSettingsControls();
 	void RenderSettingsGraphics(CUIRect MainView);
@@ -574,6 +592,7 @@ public:
 		SETTINGS_GENERAL,
 		SETTINGS_PLAYER,
 		SETTINGS_TEE,
+		SETTINGS_TEE7,
 		SETTINGS_APPEARANCE,
 		SETTINGS_CONTROLS,
 		SETTINGS_GRAPHICS,
