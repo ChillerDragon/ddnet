314c8c5e177552e7aca67254de3f7c9ff4ba7cd7
ChillerDragon <ChillerDragon@gmail.com>
2022-10-16 10:14:05 +0200

    Add 0.7 client support
    While keeping 0.6 fully working and untouched
    This adds the option to connect via 0.7
    
    0.7 servers are now mixed into the server browser list.
    And users can also manually connect via 0.7
    using a connection url like so:
    
        ./DDNet "connect tw-0.7+udp://127.0.0.1:8303"
    
    This also adds a "Tee 0.7" section in the menu to pick a 0.7 skin.
    The config variables are prefixed with player7_ like for example
    player7_color_body

--- src/engine/client/client.h
+++ src/engine/client/client.h
@@ -89,6 +89,7 @@ class CClient : public IClient, public CDemoPlayer::IListener
 	char m_aConnectAddressStr[MAX_SERVER_ADDRESSES * NETADDR_MAXSTRSIZE];
 
 	CUuid m_ConnectionID;
+	bool m_Sixup;
 
 	bool m_HaveGlobalTcpAddr = false;
 	NETADDR m_GlobalTcpAddr;
@@ -191,7 +192,7 @@ class CClient : public IClient, public CDemoPlayer::IListener
 	int m_aSnapshotIncomingDataSize[NUM_DUMMIES];
 
 	CSnapshotStorage::CHolder m_aDemorecSnapshotHolders[NUM_SNAPSHOT_TYPES];
-	char m_aaaDemorecSnapshotData[NUM_SNAPSHOT_TYPES][2][CSnapshot::MAX_SIZE];
+	char m_aaaDemorecSnapshotData[NUM_SNAPSHOT_TYPES][3][CSnapshot::MAX_SIZE];
 
 	CSnapshotDelta m_SnapshotDelta;
 
@@ -326,6 +327,7 @@ public:
 	const void *SnapFindItem(int SnapID, int Type, int ID) const override;
 	int SnapNumItems(int SnapID) const override;
 	void SnapSetStaticsize(int ItemType, int Size) override;
+	void SnapSetStaticsize7(int ItemType, int Size) override;
 
 	void Render();
 	void DebugRender();
@@ -339,6 +341,8 @@ public:
 
 	const char *LoadMap(const char *pName, const char *pFilename, SHA256_DIGEST *pWantedSha256, unsigned WantedCrc);
 	const char *LoadMapSearch(const char *pMapName, SHA256_DIGEST *pWantedSha256, int WantedCrc);
+
+	int TranslateSysMsg(int *pMsgID, bool System, CUnpacker *pUnpacker, CPacker &Packer, CNetChunk *pPacket);
 
 	void ProcessConnlessPacket(CNetChunk *pPacket);
 	void ProcessServerInfo(int Type, NETADDR *pFrom, const void *pData, int DataSize);
@@ -354,6 +358,8 @@ public:
 	bool IsDDNetInfoChanged();
 	void FinishDDNetInfo();
 	void LoadDDNetInfo();
+
+	bool IsSixup() override { return m_Sixup; };
 
 	const NETADDR &ServerAddress() const override { return *m_aNetClient[CONN_MAIN].ServerAddress(); }
 	int ConnectNetTypes() const override;
--- src/engine/shared/snapshot.h
+++ src/engine/shared/snapshot.h
@@ -58,6 +58,7 @@ public:
 
 	unsigned Crc();
 	void DebugDump();
+	int TranslateSevenToSix(CSnapshot *pSixSnapDest, class CTranslationContext &TranslationContext, float LocalTime);
 	bool IsValid(size_t ActualSize) const;
 
 	static const CSnapshot *EmptySnapshot() { return &ms_EmptySnapshot; }
@@ -83,6 +84,7 @@ private:
 		MAX_NETOBJSIZES = 64
 	};
 	short m_aItemSizes[MAX_NETOBJSIZES];
+	short m_aItemSizes7[MAX_NETOBJSIZES];
 	int m_aSnapshotDataRate[CSnapshot::MAX_TYPE + 1];
 	int m_aSnapshotDataUpdates[CSnapshot::MAX_TYPE + 1];
 	CData m_Empty;
@@ -96,9 +98,10 @@ public:
 	int GetDataRate(int Index) const { return m_aSnapshotDataRate[Index]; }
 	int GetDataUpdates(int Index) const { return m_aSnapshotDataUpdates[Index]; }
 	void SetStaticsize(int ItemType, int Size);
+	void SetStaticsize7(int ItemType, int Size);
 	const CData *EmptyDelta() const;
 	int CreateDelta(const class CSnapshot *pFrom, class CSnapshot *pTo, void *pDstData);
-	int UnpackDelta(const class CSnapshot *pFrom, class CSnapshot *pTo, const void *pSrcData, int DataSize);
+	int UnpackDelta(const class CSnapshot *pFrom, class CSnapshot *pTo, const void *pSrcData, int DataSize, bool Sixup);
 };
 
 // CSnapshotStorage
@@ -117,9 +120,17 @@ public:
 
 		int m_SnapSize;
 		int m_AltSnapSize;
+		int m_TransSnapSize;
 
 		CSnapshot *m_pSnap;
 		CSnapshot *m_pAltSnap;
+
+		CSnapshot *m_pTransSnap; // translated snap from 0.7 to 0.6
+
+		CSnapshot *AltSnap()
+		{
+			return m_pTransSnap ? m_pTransSnap : m_pAltSnap;
+		}
 	};
 
 	CHolder *m_pFirst;
@@ -130,7 +141,7 @@ public:
 	void Init();
 	void PurgeAll();
 	void PurgeUntil(int Tick);
-	void Add(int Tick, int64_t Tagtime, int DataSize, const void *pData, int AltDataSize, const void *pAltData);
+	void Add(int Tick, int64_t Tagtime, int DataSize, const void *pData, int AltDataSize, const void *pAltData, int TransDataSize, void *pTransData);
 	int Get(int Tick, int64_t *pTagtime, const CSnapshot **ppData, const CSnapshot **ppAltData);
 };
 
