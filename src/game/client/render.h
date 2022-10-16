39ce6884a9988125251e54878e5010f6f0314806
ChillerDragon <ChillerDragon@gmail.com>
2022-10-16 10:14:05 +0200

    Add 0.7 client support
    While keeping 0.6 fully working and untouched
    This adds the option to connect via 0.7
    
    0.7 servers are now mixed into the server browser list.
    And users can also manually connect via 0.7
    using a connection url like so:
    
        ./DDNet "connect tw-0.7+udp://127.0.0.1:8303"
    
    This also adds a "Tee 0.7" section in the menu to pick a 0.7 skin.
    The config variables are prefixed with player7_ like for example
    player7_color_body

--- src/game/client/render.h
+++ src/game/client/render.h
@@ -8,6 +8,7 @@
 
 #include <game/client/skin.h>
 #include <game/client/ui_rect.h>
+#include <game/generated/protocol7.h>
 
 class CAnimState;
 class CSpeedupTile;
@@ -24,6 +25,8 @@ struct CEnvPointBezier;
 struct CEnvPointBezier_upstream;
 struct CMapItemGroup;
 struct CQuad;
+
+#include <game/generated/protocol.h>
 
 class CTeeRenderInfo
 {
@@ -46,6 +49,11 @@ public:
 		m_GotAirJump = true;
 		m_TeeRenderFlags = 0;
 		m_FeetFlipped = false;
+
+		// 0.7
+		for(auto &Texture : m_aTextures)
+			Texture = IGraphics::CTextureHandle();
+		m_BotTexture = IGraphics::CTextureHandle();
 	}
 
 	CSkin::SSkinTextures m_OriginalRenderSkin;
@@ -67,6 +75,12 @@ public:
 	{
 		return m_CustomColoredSkin ? m_ColorableRenderSkin.m_Body.IsValid() : m_OriginalRenderSkin.m_Body.IsValid();
 	}
+
+	// 0.7
+	IGraphics::CTextureHandle m_aTextures[protocol7::NUM_SKINPARTS];
+	vec4 m_aColors[protocol7::NUM_SKINPARTS];
+	IGraphics::CTextureHandle m_BotTexture;
+	vec4 m_BotColor;
 };
 
 // Tee Render Flags
@@ -129,6 +143,9 @@ class CRenderTools
 	static void GetRenderTeeBodyScale(float BaseSize, float &BodyScale);
 	static void GetRenderTeeFeetScale(float BaseSize, float &FeetScaleWidth, float &FeetScaleHeight);
 
+	void RenderTee6(const CAnimState *pAnim, const CTeeRenderInfo *pInfo, int Emote, vec2 Dir, vec2 Pos, float Alpha = 1.0f) const;
+	void RenderTee7(const CAnimState *pAnim, const CTeeRenderInfo *pInfo, int Emote, vec2 Dir, vec2 Pos) const;
+
 public:
 	class IGraphics *Graphics() const { return m_pGraphics; }
 	class ITextRender *TextRender() const { return m_pTextRender; }
@@ -137,6 +154,7 @@ public:
 
 	void SelectSprite(CDataSprite *pSprite, int Flags = 0, int sx = 0, int sy = 0) const;
 	void SelectSprite(int Id, int Flags = 0, int sx = 0, int sy = 0) const;
+	void SelectSprite7(int Id, int Flags = 0, int sx = 0, int sy = 0) const;
 
 	void GetSpriteScale(const CDataSprite *pSprite, float &ScaleX, float &ScaleY) const;
 	void GetSpriteScale(int Id, float &ScaleX, float &ScaleY) const;
