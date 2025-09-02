3dea6d9490455115d3b003922e1b7c016c532e3e
ChillerDragon <chillerdragon@gmail.com>
2025-09-02 10:02:47 +0200

    Add `role_allow` rcon command
    Works similar to the already existing `access_level` command.
    But it does not store the access level on the command but the command in
    the role. This is more flexible. Using `role_allow` command access can
    be granted to helpers without giving the same access to moderators.

--- src/engine/server/authmanager.h
+++ src/engine/server/authmanager.h
@@ -24,6 +24,7 @@ class CRconRole
 {
 	char m_aName[64];
 	int m_Rank = RANK_NONE;
+	std::vector<std::string> m_vRconCommands;
 
 public:
 	// Name of the rcon role. For example "admin".
@@ -36,6 +37,9 @@ public:
 	// Roles with higher rank can see commands executed by roles with lower rank
 	// but not vice versa.
 	int Rank() const { return m_Rank; }
+
+	bool CanUseRconCommand(const char *pCommand);
+	bool AllowCommand(const char *pCommand);
 
 	CRconRole(const char *pName, int Rank) :
 		m_Rank(Rank)
@@ -76,6 +80,7 @@ public:
 	bool CheckKey(int Slot, const char *pPw) const;
 	int DefaultKey(const char *pRoleName) const;
 	int KeyLevel(int Slot) const;
+	CRconRole *KeyRole(int Slot) const;
 	const char *KeyIdent(int Slot) const;
 	bool IsValidIdent(const char *pIdent) const;
 	void UpdateKeyHash(int Slot, MD5_DIGEST Hash, const unsigned char *pSalt, const char *pRoleName);
@@ -86,6 +91,7 @@ public:
 	int NumNonDefaultKeys() const;
 	CRconRole *FindRole(const char *pName);
 	bool AddRole(const char *pName, int Rank);
+	bool CanRoleUseCommand(const char *pRoleName, const char *pCommand);
 };
 
 #endif //ENGINE_SERVER_AUTHMANAGER_H
