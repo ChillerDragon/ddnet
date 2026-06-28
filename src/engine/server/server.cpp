e8d185034ea2e32ea76b101979630156ba7d1a2f
ChillerDragon <ChillerDragon@gmail.com>
2026-06-28 16:50:51 +0200

    ssh server class

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -2967,6 +2967,7 @@ void CServer::PumpNetwork(bool PacketWaiting)
 
 	m_ServerBan.Update();
 	m_Econ.Update();
+	m_SshServer.Update();
 }
 
 void CServer::ChangeMap(const char *pMap)
@@ -3226,8 +3227,8 @@ int CServer::Run()
 	m_NetServer.SetCallbacks(NewClientCallback, NewClientNoAuthCallback, ClientRejoinCallback, DelClientCallback, this);
 
 	m_Econ.Init(Config(), Console(), &m_ServerBan);
-
 	m_Fifo.Init(Console(), Config()->m_SvInputFifo, CFGFLAG_SERVER);
+	m_SshServer.Init(Config(), Console(), &m_ServerBan);
 
 	char aBuf[256];
 	str_format(aBuf, sizeof(aBuf), "server name is '%s'", Config()->m_SvName);
@@ -3563,6 +3564,7 @@ int CServer::Run()
 	m_pRegister->OnShutdown();
 	m_Econ.Shutdown();
 	m_Fifo.Shutdown();
+	m_SshServer.Shutdown();
 	m_pHttp->Shutdown();
 	Engine()->ShutdownJobs();
 
