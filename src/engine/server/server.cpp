bc596a8e7ecbb61758d4b6219e0dcfcee0eeec51
ChillerDragon <chillerdragon@gmail.com>
2026-07-02 09:43:20 +0200

    linux only

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -3123,7 +3123,9 @@ void CServer::PumpNetwork()
 
 	m_ServerBan.Update();
 	m_Econ.Update();
+#if defined(CONF_FAMILY_UNIX)
 	m_SshServer.Update();
+#endif
 }
 
 void CServer::ChangeMap(const char *pMap)
@@ -3386,7 +3388,9 @@ int CServer::Run()
 
 	m_Econ.Init(Config(), Console(), &m_ServerBan);
 	m_Fifo.Init(Console(), Config()->m_SvInputFifo, CFGFLAG_SERVER);
+#if defined(CONF_FAMILY_UNIX)
 	m_SshServer.Init(Config(), Console(), Storage(), &m_ServerBan);
+#endif
 
 	char aBuf[256];
 	str_format(aBuf, sizeof(aBuf), "server name is '%s'", Config()->m_SvName);
@@ -3657,8 +3661,10 @@ int CServer::Run()
 					break;
 				}
 			}
+#if defined(CONF_FAMILY_UNIX)
 			if(m_SshServer.GotActiveConnections())
 				NonActive = false;
+#endif
 
 			if(NonActive)
 			{
@@ -3723,7 +3729,9 @@ int CServer::Run()
 	m_pRegister->OnShutdown();
 	m_Econ.Shutdown();
 	m_Fifo.Shutdown();
+#if defined(CONF_FAMILY_UNIX)
 	m_SshServer.Shutdown();
+#endif
 	m_pHttp->Shutdown();
 	Engine()->ShutdownJobs();
 
