deb11b820d2fc9ae9083542e6eb1e069d00b5de8
ChillerDragon <ChillerDragon@gmail.com>
2026-08-29 08:41:19 +0200

    Add -dirty suffix to git hash for untracked changes
    Changes in client only code will also affect the server and vice versa.
    Changes in the README.md not affecting the binaries at all will also show up.
    We could obtain the list of changed files which I think would be cool
    but that is a wider scope than the original issue and possibly a bit
    intrusive.
    
    Closes #10784

--- scripts/git_revision.py
+++ scripts/git_revision.py
@@ -5,7 +5,14 @@ import subprocess
 
 git_hash = os.environ.get("DDNET_GIT_SHORTREV_HASH")
 try:
-	git_hash = git_hash or subprocess.check_output(["git", "rev-parse", "--short=16", "HEAD"], stderr=subprocess.DEVNULL).decode().strip()
+	git_hash = git_hash or subprocess.check_output([
+        "git",
+        "describe",
+        "--always",
+        "--dirty",
+        "--match=''",
+        "--abbrev=16"],
+        stderr=subprocess.DEVNULL).decode().strip()
 except (FileNotFoundError, subprocess.CalledProcessError):
 	pass
 if git_hash is not None:
