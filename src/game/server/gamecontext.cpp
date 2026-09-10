0b8192555859851b3489e5adf986d0848673126f
ChillerDragon <chillerdragon@gmail.com>
2026-09-10 13:58:37 +0200

    Pass rcon role instead of auth level to on auth method
    This refactor is a preparation for the addition of more rcon roles.
    
    Helps #10681

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -4725,13 +4725,13 @@ const char *CGameContext::NetVersion() const { return GAME_NETVERSION; }
 
 IGameServer *CreateGameServer() { return new CGameContext; }
 
-void CGameContext::OnSetAuthed(int ClientId, int Level)
+void CGameContext::OnSetAuthed(int ClientId, CRconRole *pRole)
 {
-	if(m_apPlayers[ClientId] && m_VoteCloseTime && Level != AUTHED_NO)
+	if(m_apPlayers[ClientId] && m_VoteCloseTime && pRole)
 	{
 		char aBuf[512];
 		str_format(aBuf, sizeof(aBuf), "ban %s %d Banned by vote", Server()->ClientAddrString(ClientId, false), g_Config.m_SvVoteKickBantime);
-		if(!str_comp_nocase(m_aVoteCommand, aBuf) && (m_VoteCreator == -1 || Level > Server()->GetAuthedState(m_VoteCreator)))
+		if(!str_comp_nocase(m_aVoteCommand, aBuf) && (m_VoteCreator == -1 || pRole->Rank() > Server()->GetAuthedState(m_VoteCreator)))
 		{
 			m_VoteEnforce = CGameContext::VOTE_ENFORCE_NO_ADMIN;
 			Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "game", "Vote aborted by authorized login.");
@@ -4740,11 +4740,11 @@ void CGameContext::OnSetAuthed(int ClientId, int Level)
 
 	if(m_TeeHistorianActive)
 	{
-		if(Level != AUTHED_NO)
+		if(pRole)
 		{
 			m_TeeHistorian.RecordAuthLogin(
 				ClientId,
-				CAuthManager::AuthLevelToRoleName(Level),
+				pRole->Name(),
 				Server()->GetAuthName(ClientId));
 		}
 		else
