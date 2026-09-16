b8345669ad7dceea7184fe8773605275abcd7ab1
ChillerDragon <chillerdragon@gmail.com>
2026-09-16 16:10:02 +0200

    make sure it compiles without deprecation warning w old and new libssh

--- src/engine/shared/ssh_server.cpp
+++ src/engine/shared/ssh_server.cpp
@@ -2164,7 +2164,11 @@ bool CSshServer::GenerateHostKeyIfMissing()
 	log_info("ssh", "generating host key ...");
 
 	ssh_key Key = nullptr;
+#if LIBSSH_VERSION_INT >= SSH_VERSION_INT(0, 11, 0)
 	if(ssh_pki_generate_key(SSH_KEYTYPE_RSA, nullptr, &Key) != SSH_OK)
+#else
+	if(ssh_pki_generate(SSH_KEYTYPE_RSA, 0, &Key) != SSH_OK)
+#endif
 	{
 		log_error("ssh", "failed to generate host key");
 		str_copy(m_aError, "failed to generate host key");
